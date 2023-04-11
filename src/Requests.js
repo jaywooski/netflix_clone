a15729298a22a322dedb7a394544139599f27f28
John Wooley <john.m.wooley@gmail.com>
2023-04-10 19:36:54 -0500

    Adding frontend styling to project

--- src/Requests.js
+++ src/Requests.js
@@ -5,8 +5,7 @@ const requests = {
 	requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
 	requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=2`,
 	requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=horror&page=1&include_adult`,
-	requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}=<<api_key>>&language=en-US`,
+	requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US`,
 };
 
-console.log("Hey here it is" + API_KEY);
 export default requests;
